3f26b6eb87c67d452724e73475c77248a1b60653
yu0910 <32055971+yu0910@users.noreply.github.com>
2017-10-04 20:33:22 +0800

    homework

---
+++ src/HW11.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class HW11 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str =scn.nextLine();
+        int v2=scn.nextInt();
+    }
+}
